d175f14953e8605fa74f7dc76536c0e536ee7d08
Zlotnik-Ilya <ilya.zlotnik@mail.ru>
2022-09-09 19:28:54 +0300

    complied 1-9 lessons

---
+++ src/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div>
+      <a href="#">Message</a>
+      <a href="#">New feed</a>
+      <a href="#">Home</a>
+    </div>
+  )
+}
+export default Header
---
+++ src/Technologies.tsx
@@ -0,0 +1,13 @@
+const Technologies = () => {
+  return (
+    <div>
+      <ul>
+        <li>css</li>
+        <li>html</li>
+        <li>ts</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Technologies
