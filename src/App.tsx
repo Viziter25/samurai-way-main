0f9772728e5721e2b33dfe27f7160697812e9465
Zlotnik-Ilya <ilya.zlotnik@mail.ru>
2022-09-22 22:48:55 +0300

    complied 17-19 lessons on ts

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,25 @@ import s from './App.module.css';
 import {Header} from './components/Header/Header'
 import {Navbar} from './components/Navbar/Navbar';
 import {Profile} from './components/Profile/Profile';
+import {Dialogs} from './components/Dialogs/Dialogs';
+import {BrowserRouter, Route} from 'react-router-dom';
+import {News} from './components/News/News';
 
 function App() {
   return (
-    <div className={s.app_wrapper}>
-      <Header/>
-      <Navbar/>
-      <Profile/>
-    </div>
+    <BrowserRouter>
+      <div className={s.app_wrapper}>
+        <Header/>
+        <Navbar/>
+        <div className={s.wrapper_content}>
+          <Route path='/Dialogs' component={Dialogs}/>
+          <Route path='/Profile' component={Profile}/>
+          <Route path='/News' component={News}/>
+
+        </div>
+      </div>
+    </BrowserRouter>
+
   );
 }
 
